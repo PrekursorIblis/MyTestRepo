5a609a15db52b8f8ab2fa2f3c2d767c6c478fc66
prekursoriblis <grazyna.zawora@hotmail.com>
2013-10-06 16:40:00 +0200

    tabQusort and partition

---
+++ src/test/java/TestTest.java
@@ -0,0 +1,29 @@
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestTest {
+    
+    public TestTest() {
+    }
+    
+  
+    @Before
+    public void setUp() {
+        
+    }
+    
+    @org.junit.Test
+    public void testMain() {
+        System.out.println("main");
+        String[] arg = null;
+        Test.main(arg);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
