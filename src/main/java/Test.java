e2be58a6d7dbe93742101d75945ca72f2d65e958
prekursoriblis <grazyna.zawora@hotmail.com>
2013-10-06 15:12:32 +0200

    Added my files.

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Test  {
+    public static Scanner in =new Scanner(System.in);
+    int[] tab;
+    public static void main(String[] arg){
+            System.out.println("Podaj zakres danych");
+            int ilosc = in.nextInt(); 
+            
+    }
+}
